2ae91cdabf187cd9536fc75843d60042619184e3
Aleksey <rusal@bk.ru>
2022-02-11 03:22:06 +0300

    init

---
+++ src/pages/About.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page class="flex flex-center"> </q-page>
+</template>
+
+<script>
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
+import API from "../api/api";
+
+export default defineComponent({
+  name: "PageIndex",
+
+  setup() {
+    const $store = useStore();
+    const opti = computed({
+      get: () => $store.state.mes.opti,
+      set: (val) => {
+        $store.commit("mes/updateOpti", val);
+      },
+    });
+    return {
+      opti,
+    };
+  },
+});
+</script>
+<style scoped>
+.field-label {
+  vertical-align: middle;
+}
+.big-card {
+  height: 500px;
+  width: 100%;
+  margin: 20px auto;
+  padding: 20px auto;
+}
+
+.box-card {
+  max-width: 100%;
+}
+.textInput {
+  margin: 20px auto;
+  padding: 30px 24px;
+}
+.tag-item {
+  margin-right: 15px;
+}
+</style>
---
+++ src/pages/Vis.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-img
+      src="http://localhost:5000/static/wc.png"
+      spinner-color="white"
+      style="height: 600px; max-width: 600px"
+    />
+  </q-page>
+</template>
+
+<script>
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "VisIndex",
+
+  setup() {
+    const $store = useStore();
+    const opti = computed({
+      get: () => $store.state.mes.opti,
+      set: (val) => {
+        $store.commit("mes/updateOpti", val);
+      },
+    });
+    return {
+      $store,
+      opti,
+    };
+  },
+});
+</script>
+<style scoped>
+.field-label {
+  vertical-align: middle;
+}
+</style>
